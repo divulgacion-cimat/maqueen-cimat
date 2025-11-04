38b0a8cc66a477dc8c36836a7bc1bf89e0cfd184
Jesús Bertani <jbrbertani@gmail.com>
2025-11-03 21:16:29 -0600

    Exmaple of a functioning scanning algorithm for maqueen

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
-basic.forever(function () {
-	
+robot.onObstacleDistanceChanged(function () {
+    distance = robot.obstacleDistance()
+    if (scanning) {
+        if (distance > 15) {
+            scanning = 0
+            robot.motorTank(30, 30)
+        }
+    } else {
+        if (distance < 15) {
+            robot.motorTank(-30, -30)
+            robot.motorTank(0, 0)
+            robot.motorTank(-25, 25)
+            basic.pause(1000)
+            scanning = 1
+            robot.motorTank(25, -25)
+        } else {
+            robot.motorTank(30, 30)
+        }
+    }
 })
+let scanning = 0
+let distance = 0
+robot.dfRobotMaqueen.start()
